d2ad5c9cf757e7c7369ce24250562ed42c451f0c
saulgordillo <1523209@uab.cat>
2023-11-25 17:08:53 +0100

    Board tests passed

--- test/model/test_boardData.py
+++ test/model/test_boardData.py
@@ -34,6 +34,47 @@ class TestBoard(unittest.TestCase):
                     num_bombs = board.checkTile(i, j)
                     self.assertTrue(0 <= num_bombs <= 8)
 
+    def setUp(self):
+        self.board = Board(1)
+
+    # Pairwise testing
+    def test_pairwise(self):
+        self.board = Board(2)
+        self.assertEqual(len(self.board.tiles), 8)
+        self.assertEqual(len(self.board.tiles[0]), 8)
+
+    # Statement coverage
+    def test_statement_coverage(self):
+        self.board = Board(3)
+        self.assertEqual(len(self.board.tiles), 10)
+        self.assertEqual(len(self.board.tiles[0]), 10)
+
+    # Decision coverage
+    def test_decision_coverage(self):
+        with self.assertRaises(ValueError):
+            self.board = Board(4)
+
+    # Condition coverage
+    def test_condition_coverage(self):
+        self.board = Board(1)
+        self.board.tiles[0][0].is_bomb = True
+        self.assertFalse(self.board.checkTile(0, 0))
+
+    # Path coverage
+    def test_path_coverage(self):
+        self.board = Board(1)
+        self.board.tiles[0][0].is_bomb = True
+        self.board.tiles[0][0].is_revealed = True
+        self.assertFalse(self.board.checkHidden(0, 0))
+
+    # Loop testing
+    def test_loop_testing(self):
+        self.board = Board(1)
+        for i in range(self.board.size):
+            for j in range(self.board.size):
+                self.board.tiles[i][j].is_bomb = True
+        self.assertTrue(self.board.is_game_won())
+
 
 if __name__ == '__main__':
     unittest.main()
